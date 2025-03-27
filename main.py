27cc217e84bdf34dd1af905e9c5c37d2916f7597
SoloScriptSage <vladyslavhirchukbusiness@outlook.com>
2025-03-27 11:35:01 +0100

    Weights and layers implementation

--- main.py
+++ main.py
@@ -43,3 +43,26 @@ Y = np.array(Y)
 
 print(X, "\n\n", Y)
 
+# Activation function
+def sigmoid(x):
+   return (1/(1 + np.exp(-x)))
+
+# Creatnig the Feed forward neural network
+# 1 layes *INPUT (1,30)
+# 2 layer *HIDDEN (1,5)
+# 3 layer *output (3,3)
+
+def f_forward(x, w1, w2):
+   # Hidden layer
+   z1 = x.dot(w1)
+   a1 = sigmoid(z1)
+
+   # =Output layer
+   z2 = a1.dot(w2)
+   a2 = sigmoid(z2)
+
+   return (a2)
+
+# Initializing the random weights
+
+
